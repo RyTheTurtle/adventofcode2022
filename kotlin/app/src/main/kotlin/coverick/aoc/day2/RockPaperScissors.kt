3c50008d60862deb47e4feb400761875b5ba6cd9
= <=>
2022-12-05 00:22:13 -0600

    Finished Day 2 Part 2 Rock Paper Scissors game

--- kotlin/app/src/main/kotlin/coverick/aoc/day2/RockPaperScissors.kt
+++ kotlin/app/src/main/kotlin/coverick/aoc/day2/RockPaperScissors.kt
@@ -4,6 +4,8 @@ import readResourceFile
 // https://adventofcode.com/2022/day/2
 val INPUT_FILE = "rockPaperScissors-input.txt"
 
+// enumerate shapes and outcomes for rock,paper,scissors 
+// according to text rules.
 enum class Shape(val score: Int) {
     ROCK(1),
     PAPER(2), 
@@ -16,7 +18,8 @@ enum class Outcome(val score: Int){
     DRAW(3)
 }
 
-// Wire up config tables for use in scoring and parsing 
+// map input letters to shapes and outcomes for the game
+// based on rules (outcome letter mappings are from part 2)
 val SHAPE_LETTER_MAPPINGS = mapOf(
     "A" to Shape.ROCK, 
     "X" to Shape.ROCK,
@@ -26,9 +29,15 @@ val SHAPE_LETTER_MAPPINGS = mapOf(
     "Z" to Shape.SCISSOR
 )
 
+
+val OUTCOME_LETTER_MAPPING = mapOf(
+    "X" to Outcome.LOSE,
+    "Y" to Outcome.DRAW,
+    "Z" to Outcome.WIN
+)
+
 // configure scoring rules for 2 player rock,paper,scissors
-// as a multidimensional map . First choice is opponent, second 
-// choice is my choice 
+// as a multidimensional map opponent shape -> my shape -> outcome
 val TWO_PLAYER_SCORE_MAPPING = mapOf(
     Shape.ROCK to mapOf(
         Shape.ROCK to Outcome.DRAW,
@@ -50,22 +59,62 @@ val TWO_PLAYER_SCORE_MAPPING = mapOf(
 )
 
 
+/* for part 2, configure mapping of 
+opponent shape -> desired outcome -> my shape to get that outcome
+*/
+val OPPONENT_OUTCOME_CONFIG = mapOf(
+    Shape.ROCK to mapOf(
+        Outcome.DRAW to Shape.ROCK,
+        Outcome.WIN to Shape.PAPER,
+        Outcome.LOSE to Shape.SCISSOR
+    ),
+
+    Shape.PAPER to mapOf(
+        Outcome.DRAW to Shape.PAPER,
+        Outcome.WIN to Shape.SCISSOR,
+        Outcome.LOSE to Shape.ROCK
+    ),
+
+    Shape.SCISSOR to mapOf(
+        Outcome.DRAW to Shape.SCISSOR,
+        Outcome.WIN to Shape.ROCK,
+        Outcome.LOSE to Shape.PAPER 
+    )
+)
+
+fun scoreRound(opponent: Shape?, me:Shape?) : Int {
+    val outcomeScore: Int = TWO_PLAYER_SCORE_MAPPING.get(opponent)?.get(me)?.score?:0
+    val choiceScore: Int = me?.score?:0
+    return outcomeScore + choiceScore
+}
+
 fun part1(): Int{
-    // return total score for strategy guide
-    // use a lookup table for configuring scoring 
     val rounds = readResourceFile(INPUT_FILE)
     var totalScore = 0 
     rounds.forEach({
         val inputs = it.split(" ")
         val myChoice = SHAPE_LETTER_MAPPINGS.get(inputs[1])
-        val opponentChoice = SHAPE_LETTER_MAPPINGS.get(inputs[0]); 
-        val roundScore: Int = TWO_PLAYER_SCORE_MAPPING.get(opponentChoice)?.get(myChoice)?.score?:0
-        val choiceScore: Int = myChoice?.score ?:0
-        totalScore += roundScore + choiceScore 
+        val opponentChoice = SHAPE_LETTER_MAPPINGS.get(inputs[0])
+        totalScore += scoreRound(opponentChoice, myChoice)
+    })
+    return totalScore
+}
+
+
+fun part2(): Int{
+    val rounds = readResourceFile(INPUT_FILE)
+    var totalScore = 0 
+    rounds.forEach({
+        val inputs = it.split(" ")
+        val opponentChoice = SHAPE_LETTER_MAPPINGS.get(inputs[0])
+        val desiredOutcome = OUTCOME_LETTER_MAPPING.get(inputs[1])
+        val myChoice = OPPONENT_OUTCOME_CONFIG.get(opponentChoice)?.get(desiredOutcome)
+        totalScore += scoreRound(opponentChoice, myChoice)
     })
     return totalScore
 }
 
 fun solution(){
     println("Rock,Paper,Scissors Part 1 Solution: ${part1()}")
+    println("Rock,Paper,Scissors Part 2 Solution: ${part2()}")
 }
