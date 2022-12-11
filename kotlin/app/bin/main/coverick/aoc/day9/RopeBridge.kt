45637f6f12f3daf11fd23eca181af33d750a564a
= <=>
2022-12-10 23:46:57 -0600

    Day9 part 2 solution, including refactoring part 1 since both parts have the same algorithm, just different size of rope

--- kotlin/app/bin/main/coverick/aoc/day9/RopeBridge.kt
+++ kotlin/app/bin/main/coverick/aoc/day9/RopeBridge.kt
@@ -2,107 +2,124 @@ package coverick.aoc.day9
 import readResourceFile
 val INPUT_FILE = "ropeBridge-input.txt"
 
-fun part1(): Int {
-    var hPos = Pair(0,0)
-    var tPos = Pair(0,0)
-    val tailSpots = hashSetOf(tPos)
-    var instructions = readResourceFile(INPUT_FILE)
-    val adjustTail = fun(){
-        val xDelta = hPos.first - tPos.first
-        val yDelta = hPos.second - tPos.second 
-        if(xDelta > 1){
-            if(yDelta >= 1){
-                // need to move diagonal up right
-                tPos = Pair(tPos.first + 1, tPos.second + 1)
-            } else if(yDelta <= -1){
-                // need to move diagonal down right 
-                tPos = Pair(tPos.first + 1, tPos.second - 1)
-            } else {
-                // right 
-                tPos = Pair(tPos.first + 1, tPos.second)
-            }
-        } else if(xDelta < -1){
-            if(yDelta >= 1){
-                // need to move diagonal up left
-                tPos = Pair(tPos.first - 1, tPos.second + 1)
-            } else if(yDelta <= -1){
-                // need to move diagonal down left 
-                tPos = Pair(tPos.first - 1, tPos.second - 1)
-            } else {
-                // left 
-                tPos = Pair(tPos.first - 1, tPos.second)
-            }
-        } else if(yDelta > 1){
-            if(xDelta >= 1){
-                // need to move diagonal up right
-                tPos = Pair(tPos.first + 1, tPos.second + 1)
-            } else if(xDelta <= -1){
-                // need to move diagonal up left 
-                tPos = Pair(tPos.first - 1, tPos.second + 1)
-            } else {
-                // up 
-                tPos = Pair(tPos.first, tPos.second + 1 )
-            } 
-        } else if(yDelta < -1){
-            if(xDelta >= 1){
-                // need to move diagonal down right
-                tPos = Pair(tPos.first + 1, tPos.second - 1)
-            } else if(xDelta <= -1){
-                // need to move diagonal down left 
-                tPos = Pair(tPos.first - 1, tPos.second - 1)
-            } else {
-                // down
-                tPos = Pair(tPos.first, tPos.second - 1 )
-            } 
+/**
+ * given a head and tail coordinate, move the tail appropriately 
+ * such that the tail is adjacent to the head. assumption: the head is not 
+ * more than one move away from the tail. Otherwise we'd just repeat this function 
+ * until we got to a state where the tail doesn't need to move. 
+ * 
+ * returns: the pair of ints representing the coordinate for the tail after adjustment
+ */
+val adjustTail = fun(head: Pair<Int,Int>, tail: Pair<Int,Int>):Pair<Int,Int>{
+    val xDelta = head.first - tail.first
+    val yDelta = head.second - tail.second 
+    var tPos   = tail 
+    if(xDelta > 1){
+        if(yDelta >= 1){
+            // need to move diagonal up right
+            tPos = Pair(tPos.first + 1, tPos.second + 1)
+        } else if(yDelta <= -1){
+            // need to move diagonal down right 
+            tPos = Pair(tPos.first + 1, tPos.second - 1)
+        } else {
+            // right 
+            tPos = Pair(tPos.first + 1, tPos.second)
         }
-        tailSpots.add(tPos)
+    } else if(xDelta < -1){
+        if(yDelta >= 1){
+            // need to move diagonal up left
+            tPos = Pair(tPos.first - 1, tPos.second + 1)
+        } else if(yDelta <= -1){
+            // need to move diagonal down left 
+            tPos = Pair(tPos.first - 1, tPos.second - 1)
+        } else {
+            // left 
+            tPos = Pair(tPos.first - 1, tPos.second)
+        }
+    } else if(yDelta > 1){
+        if(xDelta >= 1){
+            // need to move diagonal up right
+            tPos = Pair(tPos.first + 1, tPos.second + 1)
+        } else if(xDelta <= -1){
+            // need to move diagonal up left 
+            tPos = Pair(tPos.first - 1, tPos.second + 1)
+        } else {
+            // up 
+            tPos = Pair(tPos.first, tPos.second + 1 )
+        } 
+    } else if(yDelta < -1){
+        if(xDelta >= 1){
+            // need to move diagonal down right
+            tPos = Pair(tPos.first + 1, tPos.second - 1)
+        } else if(xDelta <= -1){
+            // need to move diagonal down left 
+            tPos = Pair(tPos.first - 1, tPos.second - 1)
+        } else {
+            // down
+            tPos = Pair(tPos.first, tPos.second - 1 )
+        } 
     }
+    return tPos
+}
 
+val getTail = fun(snake: ArrayList<Pair<Int,Int>>):Pair<Int,Int>{
+    return snake.get(snake.size - 1)
+}
+
+val getSnake = fun(size:Int): ArrayList<Pair<Int,Int>> { 
+    val snake = ArrayList<Pair<Int,Int>>()
+    for(i in 1..size){
+        snake.add(Pair(0,0))
+    }
+    return snake 
+}
+
+/**
+ * process a set of move instructions and track the coordinates where the snake tail has landed
+ * return the set of unique coordinates the snake tail has touched. 
+ */
+val trackSnakeTail = fun(snake: ArrayList<Pair<Int,Int>>, instructions: List<String>): Set<Pair<Int,Int>> {
+    val tailSpots = hashSetOf(getTail(snake))
     for(instruction in instructions){
         var direction = instruction.split(" ")[0]
-        var magnitude = instruction.split(" ")[1].toInt()
-        println("Head ${hPos} Tail ${tPos} Instruction ${instruction}")
-        println("\tTail Positions Visited ${tailSpots}")
-        when(direction){
-            "R" -> {
-                for(i in 1..magnitude){
-                    hPos = Pair(hPos.first + 1, hPos.second)
-                    adjustTail()
-                }
+        var magnitude = instruction.split(" ")[1].toInt() 
+        var hPos = snake.get(0)
+        
+        for(i in 1..magnitude){
+            when(direction){
+                "R" -> snake.set(0, Pair(hPos.first + 1, hPos.second))
+                "L" -> snake.set(0, Pair(hPos.first - 1, hPos.second))
+                "U" -> snake.set(0, Pair(hPos.first, hPos.second + 1))
+                "D" -> snake.set(0, Pair(hPos.first, hPos.second - 1))
             }
-            "L" -> {
-                for(i in 1..magnitude){
-                    hPos = Pair(hPos.first - 1, hPos.second)
-                    adjustTail()
-                }
+            hPos = snake.get(0)
+            for(j in 1..snake.size-1){
+                // println("${snake}")
+                snake.set(j, adjustTail(snake.get(j-1),snake.get(j)))
             }
-            "U" -> {
-                for(i in 1..magnitude){
-                    hPos = Pair(hPos.first, hPos.second + 1)
-                    adjustTail()
-                }
-
-            }
-            "D" -> {
-                for(i in 1..magnitude){
-                    hPos = Pair(hPos.first, hPos.second - 1)
-                    adjustTail()
-                }
-            }
+            tailSpots.add(getTail(snake))
         }
-        tailSpots.add(tPos)
-        println("Total tail spots visited: ${tailSpots.size}")
-    }
-    println("\tTail Positions Visited ${tailSpots}")
-    return tailSpots.size
+    } 
+    return tailSpots
+}
+
+fun part1(): Int {
+    var instructions = readResourceFile(INPUT_FILE)
+    val SNAKE_LENGTH = 2
+    val snake = getSnake(SNAKE_LENGTH)
+    return trackSnakeTail(snake,instructions).size
 }
 
 fun part2(): Int {
-    return 0 
+    // same as part 1, just with a longer snake
+    var instructions = readResourceFile(INPUT_FILE)
+    val SNAKE_LENGTH = 10
+    val snake = getSnake(SNAKE_LENGTH)
+    return trackSnakeTail(snake,instructions).size 
 }
+
 
 fun solution(){
     println("Rope Bridge Part 1 Solution ${part1()}")
     println("Rope Bridge Part 2 Solution ${part2()}")
-
 }
