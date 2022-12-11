e7807fa276413f8f34f24540a511190070105a92
= <=>
2022-12-11 00:42:49 -0600

    Day 10 part 2

--- kotlin/app/src/main/kotlin/coverick/aoc/day10/CathodeRayTube.kt
+++ kotlin/app/src/main/kotlin/coverick/aoc/day10/CathodeRayTube.kt
@@ -2,11 +2,37 @@ package coverick.aoc.day10
 import readResourceFile
 val INPUT_FILE = "cathodeRayTube-input.txt"
 
+class CRT(x:Int) {
+    var clock: Int 
+    var pixels: ArrayList<ArrayList<Char>>
+    val WIDTH = x
+    init {
+        clock = 1
+        pixels = ArrayList<ArrayList<Char>>() 
+        pixels.add(ArrayList<Char>())
+    }
 
-class CPU {
+    fun draw(sprite: Int) {
+        val row = clock / WIDTH 
+        if(row > pixels.size -1 ){
+            pixels.add(ArrayList<Char>())
+        }
+        val col = clock % WIDTH
+        val isLit = col >= sprite-1 && col <= sprite+1
+        if(isLit){
+            pixels.get(row).add('#')
+        } else {
+            pixels.get(row).add('.')
+        }
+        clock++
+    }
+}
+
+class CPU(crt: CRT) {
     var registerValue = 1
     var clock = 1
     val importantCycles = setOf(20,60,100,140,180,220)
+    val crt = crt 
     
     // returns non null if during this cmd we have an important cycle needed
     fun execute(cmd: String): Int? {
@@ -18,6 +44,7 @@ class CPU {
                 if(clock in importantCycles){ 
                     result = registerValue * clock
                 }
+                crt.draw(registerValue)
             }
             "addx" -> {
                 val value = cmd.split(" ")[1].toInt()
@@ -25,11 +52,13 @@ class CPU {
                 if(clock in importantCycles){ 
                     result = registerValue * clock
                 }
+                crt.draw(registerValue)
                 registerValue += value 
                 clock += 1
                 if(clock in importantCycles){ 
                     result = registerValue * clock
                 }
+                crt.draw(registerValue)
             }
         }
         return result
@@ -38,7 +67,8 @@ class CPU {
 
 fun part1(): Int {
     val instructions = readResourceFile(INPUT_FILE)
-    val cpu = CPU()
+    val crt = CRT(40)
+    val cpu = CPU(crt)
     var sum = 0
     for(cmd in instructions){
        val res = cpu.execute(cmd)
@@ -49,12 +79,20 @@ fun part1(): Int {
     return sum
 }
 
-fun part2(): Int {
-    return 0 
+fun part2(): CRT {
+    val instructions = readResourceFile(INPUT_FILE)
+    val crt = CRT(40)
+    val cpu = CPU(crt)
+    instructions.forEach{cpu.execute(it)}
+    return crt
 }
 
 fun solution(){
     println("Cathode-Ray Tube Solution Part 1 Solution ${part1()}")
-    println("Cathode-Ray Tube Solution Part 2 Solution ${part2()}")
+    println("Cathode-Ray Tube Solution Part 2 Solution")
+    val part2 = part2()
+    for(row in part2.pixels){
+        println("\t${row.joinToString("")}")
+    }
 
 }
